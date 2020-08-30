5c0969e8bb1c0b6182173948da652139bd2846b3
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-08-30 18:11:28 +0900

    fix use App\request

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookRequest;
 use App\Book;
 
 class BookController extends Controller
