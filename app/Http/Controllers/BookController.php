bc57c94b28b7225ad76343377da35ff0c849de80
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-08-30 16:56:03 +0900

    add update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -17,4 +17,13 @@ class BookController extends Controller
         $book = Book::findOrFail($id);
         return view('book/edit', compact('book'));
     }
+
+    public function update(Request $request, $id) {
+        $book = Book::findOrFail($id);
+        $book->name = $request->name;
+        $book->price = $request->price;
+        $book->author = $request->author;
+        $book->save();
+        return redirect("/book");
+    }
 }
