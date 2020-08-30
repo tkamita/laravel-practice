7a6089c2f719d7640daccef55e71e6fb200cf69c
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-08-30 16:06:52 +0900

    add book edit view

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('book', 'BookController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('book', 'BookController');
